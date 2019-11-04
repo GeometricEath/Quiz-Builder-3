8e200b36f245bc3e0c7e07cfd1a66fbbe7a34703
Geometr <geo.fsv.p@gmail.com>
2019-11-04 03:18:08 +0300

    + builder + FS +open view

---
+++ src/modules/fileSistem.js
@@ -0,0 +1,141 @@
+/* eslint-disable no-console */
+import path from 'path';
+import { remote } from 'electron';
+// import { createQuize } from "../modules/QuizeBuilder.js";
+// import parser from './xmlparser'
+const fs = require('fs');
+const util = require('util');
+const mime = require('mime/lite');
+const writeFile = util.promisify(fs.writeFile);
+const mkdir = util.promisify(fs.mkdir);
+
+export default class FileSistem {
+    constructor() {
+
+    }
+    saveProject(questionsArray, quizeName) {
+        let outputPath = remote.dialog.showOpenDialog({
+            properties: ['openDirectory']
+        });
+        outputPath = outputPath.toString();
+        let projectPath = path.join(outputPath, quizeName);
+
+        function saveFile(data) {
+            return readUrlBlob(data.img)
+                .then((obj) => {
+                    let extension = mime.getExtension(obj.type);
+                    let imgName = 'img' + data.id + '.' + extension;
+                    data.path = path.join('don', quizeName, imgName);
+                    return writeFile(path.join(projectPath, imgName), obj.buffer)
+                })
+                .catch(er => { console.error(er) })
+                .then(() => {
+                    console.log('Save');
+                })
+
+        }
+        function readUrlBlob(url) {
+            return new Promise(function (resolve, reject) {
+                let xhr = new XMLHttpRequest();
+                xhr.open('GET', url, true);
+                xhr.responseType = 'blob';
+                xhr.onload = function () {
+                    if (this.status == 200) {
+                        let blob = this.response;
+                        console.log(blob.type);
+                        let reader = new FileReader();
+                        reader.readAsArrayBuffer(blob);
+                        reader.onloadend = (evt => {
+                            let buffer = Buffer.from(new Uint8Array(evt.target.result))
+                            console.log('Чтение блоба' + url);
+                            resolve({ buffer: buffer, type: blob.type });
+                        });
+                    } else { reject(Error(xhr.statusText)) }
+                };
+                xhr.send();
+            })
+        }
+
+        mkdir(projectPath)
+            .then(() => {
+                return Promise.all(questionsArray.map(saveFile))
+
+            })
+            .then(() => console.log('End'))
+            .then(() => {
+                // let xmlData = createQuize(questionsArray, quizeName);
+                // let xmlPath = path.join(projectPath, 'quize.xml')
+                // return writeFile(xmlPath, xmlData);
+            }).catch((err) => {
+                console.log('Ошибка! ' + err);
+            }).then(() => console.log('Quiz project seved'))
+
+        return
+        // mkdir(projectPath)
+        //     .then(() => {
+        //         console.log('Папка создана ' + projectPath);
+        //         function sequencer(sequence, question) {
+        //             return sequence.then(() => {
+        //                 return this.readUrlBlob(question.img);
+
+        //             })
+        //                 .then((obj) => {
+        //                     let extension = mime.getExtension(obj.type);
+        //                     let imgName = 'img' + question.id + '.' + extension;
+        //                     question.path = path.join('don', quizeName, imgName);
+        //                     return writeFile(path.join(projectPath, imgName), obj.buffer);
+        //                 })
+        //                 .catch((er) => {
+        //                     console.error('Ошибка блока write blob ' + er);
+        //                 })
+        //                 .then(() => {
+        //                     console.log('Saved')
+        //                 })
+        //         }
+        //         return questionsArray.reduce(sequencer.bind(this), Promise.resolve('test'));
+        //     }).then(() => { console.log('End ') })
+        //     .then(() => {
+        //         let xmlData = createQuize(questionsArray, quizeName);
+        //         let xmlPath = path.join(projectPath, 'quize.xml')
+        //         return writeFile(xmlPath, xmlData);
+        //     }).catch((err) => {
+        //         console.log('Ошибка! ' + err);
+        //     });
+    }
+    openImg() {
+        return new Promise(function (resolve, reject) {
+            remote.dialog.showOpenDialog(path => {
+                console.log(path);
+                fs.readFile(path[0], (err, data) => {
+                    if (err) {
+                        reject('Ошибка при чтении img ' + err);
+                    }
+                    let arrayBuffer = [];
+                    arrayBuffer.push(data);
+                    let mimeType = mime.getType(path[0])
+                    let blob = new Blob(arrayBuffer, { type: mimeType });
+                    console.log(blob.type);
+                    resolve(window.URL.createObjectURL(blob));
+                })
+            })
+        })
+    }
+    openQuiz() {
+        return new Promise(function (resolve, reject) {
+            remote.dialog.showOpenDialog(path => {
+                console.log(path);
+                fs.readFile(path[0], (err, data) => {
+                    if (err) reject('Ошибка при чтении xml ' + err);
+                    // parser.parseXML(data)
+                    // .then((quiz)=>{
+                    //     console.log(quiz);
+                        // resolve (quiz);
+                        resolve (data);
+
+                    // })
+                })
+            })
+        })
+    }
+}
+
---
+++ src/views/OpenProject.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="select_wrapper">
+        <button @click="$router.push({name: 'landing'})">Назад</button>
+        <div class="select__file">
+            <h1>Выберете XML файл викторины</h1>
+            <button @click="open">Открыть</button>
+        </div>
+    </div>
+</template>
+<script>
+/* eslint-disable no-console */
+import FileSistem from "../modules/fileSistem.js";
+const FS = new FileSistem();
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    open() {
+      FS.openQuiz()
+        .then((quiz) => {
+          console.log('Open quiz data after parse: ' + quiz);
+          this.$router.push({
+            name: "editor",
+            params: quiz,
+          });
+        })
+        .then(() => console.log("Переход в QizeEditor"))
+        .catch(err => {
+          console.error("Ошибка чтения xml " + err);
+        });
+    }
+  }
+};
+</script>
