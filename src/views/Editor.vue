ab0d1f2498def271bb10a15f53fa5b1cef9a8c7b
Geometr <geo.fsv.p@gmail.com>
2019-11-12 16:37:57 +0300

    fix appheder

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,27 +1,29 @@
 <template>
-  <!-- <v-content>
-    <app-header></app-header> -->
-     
-  <v-content fill-height class="align-end justify-center">
-    <v-row>
-      <v-col>
+  <div class="d-flex align-end justify-center">
+    <v-content fill-height>
+      <!-- <app-header></app-header> -->
+      <v-row class="align-self-start">
         <question-list></question-list>
-        <question-form class="justify-center"></question-form>
-      </v-col>
-    </v-row>
-  </v-content>
+      </v-row>
+      <v-row>
+        <v-col>
+          <question-form class="justify-center"></question-form>
+        </v-col>
+      </v-row>
+    </v-content>
+  </div>
 </template>
 
 <script>
-import AppHeader from '../components/AppHeader.vue'
+// import AppHeader from "../components/AppHeader.vue";
 import QuestionList from "@/components/QuestionList.vue";
 import QuestionForm from "@/components/QuestionForm.vue";
 
 export default {
   components: {
-    AppHeader,
+    // AppHeader,
     QuestionList,
-    QuestionForm,
+    QuestionForm
   }
 };
 </script>
