f473efc726a00d509d388a82a2e2eb8da44e3bca
Geometr <geo.fsv.p@gmail.com>
2019-12-10 00:58:57 +0300

    parseXML работает

--- src/modules/xmlparser.js
+++ src/modules/xmlparser.js
@@ -13,7 +13,6 @@ class ParseXmlToQuiz {
 
         // xml = iconv.encode(iconv.decode(xml, 'win1251'), 'utf8');
         let parser = new XmlParser()
-        // console.log(parser.parseStringSync(xml));
         try {
             const result = parser.parseStringSync(xml);
             let json = result.root.questions[0].group[0].question;
@@ -27,31 +26,12 @@ class ParseXmlToQuiz {
                 newQestion.question = question.text[0];
                 newQestion.answers = question.answer;
                 newQestion.id = id++;
+                return newQestion;
             })
             console.log(questions, quizName);
         } catch (err) {
             console.error(err);
         }
-        // return new Promise((resolve, reject) => {
-        //     parser.parseStringSync(xml, (err, result) => {
-        //         if (err) reject(err);
-        //         // console.dir(result);
-        //         let json = result.root.questions[0].group[0].question;
-        //         let quizName = result.root.quizes["0"].quiz["0"].$.name;
-        //         let id = 0;
-        //         let questions = json.map((question) => {
-        //             let newQestion = {};
-        //             newQestion.true_answer = question.$.answer;
-        //             newQestion.timeout = question.$.timeout;
-        //             newQestion.pic_path = question.picture[0];
-        //             newQestion.question = question.text[0];
-        //             newQestion.answers = question.answer;
-        //             newQestion.id = id++;
-        //             return newQestion;
-        //         })
-        //         resolve({ questions: questions, name: quizName });
-        //     })
-        // })
     }
 }
 export default new ParseXmlToQuiz;
